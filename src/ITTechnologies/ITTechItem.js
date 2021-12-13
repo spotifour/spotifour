b090705ebdebf011bc3e6142c28c803d2b8893a5
vutuanlinh2k2 <vutuanlinh2002@gmail.com>
2021-12-13 14:33:04 +0700

    add real text for itTech section

--- src/ITTechnologies/ITTechItem.js
+++ src/ITTechnologies/ITTechItem.js
@@ -1,14 +1,33 @@
-import React from "react";
+import React, { useMemo } from "react";
 import "./ITTechnologies.css";
 
-function ITTechItem({technology, description, image}) {
+function ITTechItem({ technology, description, image }) {
+  const itemDescription = useMemo(
+    () =>
+      Object.keys(description).map((item) => {
+        return (
+          <>
+            <h3 className="ITTechItemSection">{item}</h3>
+            {description[item].map((paragraph) => {
+              return (
+                <>
+                  <p className="ITTechDescription">{paragraph}</p>
+                  <br />
+                </>
+              );
+            })}
+          </>
+        );
+      }),
+    [description]
+  );
   return (
-    <div className='ITTechItem'>
-      <h3 className='ITTechName'>{technology}</h3>
-      <img className='ITTechImage' src={image} alt={technology} />
-      <p className='ITTechDescription'>{description}</p>
+    <div className="ITTechItem">
+      <h3 className="ITTechName">{technology}</h3>
+      <img className="ITTechImage" src={image} alt={technology} />
+      {itemDescription}
     </div>
   );
 }
 
-export default ITTechItem; 
+export default ITTechItem;
